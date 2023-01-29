ffec328cf15cf101a516d85b97cf12e1dae407af
Daniele Matta <daniele.matta@aicof.it>
2023-01-29 20:39:26 +0100

    initialize project

---
+++ app-hit-mole-version0.0/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app-hit-mole-version0.0';
+}
